44d8425352a168a0355bc0c906178d4d2245b639
KelvinCL <39067792+KelvinCL@users.noreply.github.com>
2019-06-17 00:13:26 -0300

    Questão 4 feita...
    ...e testada.

--- BD1 - TURMA 01 - GRUPO 6 - PARTE 3.sql
+++ BD1 - TURMA 01 - GRUPO 6 - PARTE 3.sql
@@ -15,3 +15,11 @@ CREATE VIEW func_sal_menor_que_media AS
   SELECT *
   FROM FUNCIONARIO
   WHERE (salario < (SELECT AVG(SALARIO) FROM FUNCIONARIO));
+
+
+-- 4ª questão
+
+SELECT * FROM RESERVA 
+   WHERE (EXTRACT(day FROM dia_check_out) = (EXTRACT(day FROM dia_check_in) + 1) AND 
+         EXTRACT(month FROM dia_check_in)  = EXTRACT(month FROM dia_check_out) 
+         AND EXTRACT(year FROM dia_check_in)  = EXTRACT(year FROM dia_check_out));
